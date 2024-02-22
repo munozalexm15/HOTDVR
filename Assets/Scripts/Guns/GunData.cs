e6961768931e122c100a162e711cc2aae1f9293b
dcurto <dcurtor@ies-sabadell.cat>
2024-02-22 21:21:18 +0100

    added muzzle flash, bullet mechanic and bullet ejector

---
+++ Assets/Scripts/Guns/GunData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunData : MonoBehaviour
+{
+
+    public float firerate;
+    public float bulletSpeed;
+    public int bulletsPerMagazine;
+    public float reloadTime;
+    public float accuracy;
+
+    public string EjectAnimation;
+    public AnimationClip RightHandTriggerPull;
+    public AnimationClip LeftHandTriggerPull;
+}
