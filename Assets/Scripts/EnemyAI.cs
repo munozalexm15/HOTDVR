bc3933fa2805120c040d9e4a6a34de6a2a3e425f
Alejandro Casanova <acasanovao@ies-sabadell.cat>
2024-03-05 20:29:09 +0100

    added melee combat for player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -11,21 +11,20 @@ public class EnemyAI : MonoBehaviour
     public Transform Enemy;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         EnemyNav.SetDestination(Player.position);
-
+      
         float Distance = Vector3.Distance(Player.position, Enemy.position);
 
-        if (Distance < 1f) 
+        if (Distance < 1f)
         {
-            Debug.Log("Enemigo ataca a Melee"); 
+            Debug.Log("Enemigo ataca a Melee");
         }
-
     }
 
 }
